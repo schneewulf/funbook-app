cea92934637bda6e267678a0522957877a5f9d71
Adrian <yohanesadrian97@gmail.com>
2019-06-17 14:54:47 +0700

    adding react native avaatar and swiper

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {Avatar, Piece} from 'react-native-avataaars';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -8,9 +9,34 @@ export default class SettingsScreen extends React.Component {
 
   render() {
     return (
-      <View>
-        <Text>Home</Text>
+      <View style={styles.root}>
+        <Avatar
+          size={200}
+          avatarStyle="Circle"
+          topType="LongHairStraight"
+          accessoriesType="Prescription02"
+          hairColor="Black"
+          facialHairType="Blank"
+          clotheType="BlazerShirt"
+          eyeType="Happy"
+          eyebrowType="Default"
+          mouthType="Default"
+          skinColor="Light"
+          style={styles.avatarContainer}
+        />
+        <View>
+          <Text>Name</Text>
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  avatarContainer: {alignItems: 'center', alignSelf: 'center'},
+});
